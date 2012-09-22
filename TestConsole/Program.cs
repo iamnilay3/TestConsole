8596ed22e98f61104d7ea85d4b8236a9c399c747
Jason Shantz <jshantz@gmail.com>
2012-09-21 22:39:33 -0700

    More testing.

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -11,6 +11,7 @@ namespace TestConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World! Again!");
         }
     }
 }
